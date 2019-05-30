f20634bf723fd60130b5cd95db61260d425a9604
Rolf Baitinger <rolf@baitinger.info>
2019-05-30 20:52:46 +0200

    Bereinigungen

--- CalculatorFactory/Program.cs
+++ CalculatorFactory/Program.cs
@@ -2,10 +2,14 @@
 
 namespace CalculatorFactory
 {
+    /// <summary>
+    /// Composition Root
+    /// </summary>
     class Program
     {
         static void Main()
         {
+            //Parametereingabe
             Console.WriteLine("Enter first number");
             string input = Console.ReadLine();
             double num1, num2;
@@ -24,8 +28,10 @@ namespace CalculatorFactory
                 return;
             }
             Console.WriteLine("Enter Add, Sub, Mul or Div");
+            //Initialisierung eines Factory Objekts
             CalculateFactory factory = new CalculateFactory();
             ICalculate obj = factory.GetCalculation(Console.ReadLine());
+
             if (obj != null)
             {
                 obj.Calculate(num1, num2);
